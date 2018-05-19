f23d6301967f5f93b172694508d52b1e493aa9f1
RaiderMav <raidermaverick@gmail.com>
2018-05-19 18:50:07 -0500

    routes

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express'),
-  app = express(),
-  port = process.env.PORT || 5000
+  port = process.env.PORT || 5000,
+  mongoose = require('mongoose'),
+  keys = require('./config/keys'),
+  cookieSession = require('cookie-session'),
+  passport = require('passport')
 
-app.get('/', (req, res) => {
-  res.send({bye: `buddy`})
-})
+require('./models/user')
+
+require('./services/passport')
+mongoose.connect(keys.mongoURI)
+const app = express()
+
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey]
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/authRoutes')(app)
 
 app.listen(port, () => {
   console.log(`Server listening on port:${port}`)
---
+++ models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose'),
+  {Schema} = mongoose,
+
+  userSchema = new Schema({
+    googleId: String
+  })
+
+mongoose.model('users', userSchema)
