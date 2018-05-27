090dbf0149ec43713f0bc0e355d751e0adad60ab
RaiderMav <raidermaverick@gmail.com>
2018-05-27 15:24:10 -0500

    route diagnosis

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -18,6 +18,7 @@ module.exports = app => {
   app.get(
     '/api/logout',
      (req, res) => {
+       console.log(req.user)
        req.logout()
        res.redirect('/')
      })
@@ -25,6 +26,7 @@ module.exports = app => {
   app.get(
     '/api/current_user',
     (req, res) => {
+      console.log(req.user)
       res.send(req.user)
     // res.send(req.session)
     })
