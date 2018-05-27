4996290ad3ab5e1fba7337a44e2ae174d99ecda9
RaiderMav <raidermaverick@gmail.com>
2018-05-27 15:34:43 -0500

    fix heroku config variables

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -18,7 +18,6 @@ module.exports = app => {
   app.get(
     '/api/logout',
      (req, res) => {
-       console.log(req.user)
        req.logout()
        res.redirect('/')
      })
@@ -26,7 +25,6 @@ module.exports = app => {
   app.get(
     '/api/current_user',
     (req, res) => {
-      console.log(req.user)
       res.send(req.user)
     // res.send(req.session)
     })
