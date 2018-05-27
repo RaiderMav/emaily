c8167c9e403327d123b8ade6e3640e7cb27e0edb
RaiderMav <raidermaverick@gmail.com>
2018-05-27 13:22:59 -0500

    fix path in server/index.js

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const express = require('express'),
   keys = require('./config/keys'),
   cookieSession = require('cookie-session'),
   passport = require('passport'),
-  bodyParser = require('body-parser'),
-  path = require('path')
+  bodyParser = require('body-parser')
 
 require('./models/user')
 
@@ -32,6 +31,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
     // Express will serve up the index.html file
     // if it doesn't recognize the route
+  const path = require('path')
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
