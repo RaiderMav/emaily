98e78dbe47b3bf3ab7caa5b7824361b98a99ef13
RaiderMav <raidermaverick@gmail.com>
2018-05-27 17:36:56 -0500

    refactor renderContent

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,37 +4,36 @@ import {Link} from 'react-router-dom'
 import Payments from './Payments'
 
 class Header extends Component {
-    renderContent = ()=>{
-        switch(this.props.auth){
-            case null:
-                return ``
-            case false:
-                return <li>
-                    <a href="/auth/google">Login With Google</a>
-                </li>
-            default:
-            return (
-               [<li key="1" style={{margin:'0 10px'}}>Credits:{this.props.auth.credits}</li>,
-                <li key="2"><Payments/></li>,
-                <li key="3"><a href="/api/logout">Logout</a></li>]
-                
-        
-           )
-        }
+  renderContent () {
+    switch (this.props.auth) {
+      case null:
+        return ``
+      case false:
+        return <li>
+          <a href='/auth/google'>Login With Google</a>
+        </li>
+      default:
+        return (
+          [<li key='1' style={{margin: '0 10px'}}>Credits:{this.props.auth.credits}</li>,
+            <li key='2'><Payments /></li>,
+            <li key='3'><a href='/api/logout'>Logout</a></li>]
+
+        )
     }
+  }
   render () {
     return (
 
       <nav>
         <div className='nav-wrapper'>
-          <Link 
-          to={this.props.auth ? '/surveys' : '/'} 
-          className='left brand-logo'
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className='left brand-logo'
           >
           Emaily
           </Link>
           <ul className='right'>
-          {this.renderContent()}
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
