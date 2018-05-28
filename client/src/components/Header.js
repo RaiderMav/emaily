41e41ed90956134ae83336efbff72039e4c6c067
RaiderMav <raidermaverick@gmail.com>
2018-05-27 19:39:13 -0500

    fix header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ import Payments from './Payments'
 
 class Header extends Component {
   renderContent =()=> {
-    switch (props.auth) {
+    switch (this.props.auth) {
       case null:
         return ``
       case false:
@@ -14,7 +14,7 @@ class Header extends Component {
         </li>
       default:
         return (
-          [<li key='1' style={{margin: '0 10px'}}>Credits:{props.auth.credits}</li>,
+          [<li key='1' style={{margin: '0 10px'}}>Credits:{this.props.auth.credits}</li>,
             <li key='2'><Payments /></li>,
             <li key='3'><a href='/api/logout'>Logout</a></li>]
 
