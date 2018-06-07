ad3aedf33c3c74e932086a551cd03e65bb5f17bd
RaiderMav <raidermaverick@gmail.com>
2018-06-07 17:48:25 -0500

    started dashboard.js

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -6,6 +6,10 @@ const mongoose = require('mongoose'),
   Survey = mongoose.model('surveys')
 
 module.exports = app => {
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send(`Thank you for voting`)
+  })
+
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body,
 
